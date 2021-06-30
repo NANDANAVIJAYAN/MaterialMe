bf140a8e3f9a97ee7e5a2fd8a8208fae61ade461
Nandana <nvnair2@gmail.com>
2021-06-30 22:36:38 +0530

    MaterialMe

--- app/src/main/java/com/example/android/materialme/MainActivity.java
+++ app/src/main/java/com/example/android/materialme/MainActivity.java
@@ -19,9 +19,11 @@ package com.example.android.materialme;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -36,27 +38,6 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private ArrayList<Sport> mSportsData;
     private SportsAdapter mAdapter;
-    ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper
-            .SimpleCallback(
-            ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |
-            ItemTouchHelper.DOWN | ItemTouchHelper.UP,
-            ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-        @Override
-        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-            int from = viewHolder.getAdapterPosition();
-            int to = target.getAdapterPosition();
-
-            Collections.swap(mSportsData,from,to);
-            mAdapter.notifyItemMoved(from,to);
-            return true;
-        }
-
-        @Override
-        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-            mSportsData.remove(viewHolder.getAdapterPosition());
-            mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
-        }
-    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,9 +47,6 @@ public class MainActivity extends AppCompatActivity {
         // Initialize the RecyclerView.
         mRecyclerView = findViewById(R.id.recyclerView);
 
-        // Set the Layout Manager.
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         // Initialize the ArrayList that will contain the data.
         mSportsData = new ArrayList<>();
 
@@ -76,8 +54,38 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new SportsAdapter(this, mSportsData);
         mRecyclerView.setAdapter(mAdapter);
 
+        int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
+
+        // Set the Layout Manager.
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this,gridColumnCount));
+
         // Get the data.
         initializeData();
+        int swipeDirs;
+        if(gridColumnCount > 1) swipeDirs = 0;
+        else swipeDirs = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper
+                .SimpleCallback(
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |
+                        ItemTouchHelper.DOWN | ItemTouchHelper.UP,
+                swipeDirs) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                int from = viewHolder.getAdapterPosition();
+                int to = target.getAdapterPosition();
+
+                Collections.swap(mSportsData,from,to);
+                mAdapter.notifyItemMoved(from,to);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                mSportsData.remove(viewHolder.getAdapterPosition());
+                mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+        });
+
         helper.attachToRecyclerView(mRecyclerView);
     }
 
@@ -104,4 +112,7 @@ public class MainActivity extends AppCompatActivity {
         mAdapter.notifyDataSetChanged();
     }
 
+    public void resetSports(View view) {
+        initializeData();
+    }
 }
